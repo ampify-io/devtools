369eb37b5384763d60ca1625c888e396bbab59a3
alonronin <alon@ronin.co.il>
2020-04-11 17:00:33 +0300

    default port to 2310

--- projects/extension/src/js/config.js
+++ projects/extension/src/js/config.js
@@ -1,5 +1,5 @@
 export default {
-  AMPIFY_LOCAL_JSON: 'http://localhost:2311/ampify.json',
+  AMPIFY_LOCAL_JSON: 'http://localhost:2310/ampify.json',
   AMPIFY_LOCAL_SAVE: 'http://localhost:2310/save',
   AMPIFY_PATH: 'https://dev.staging.ampify.io/generate',
 };
