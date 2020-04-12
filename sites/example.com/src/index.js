3462eb7088b182b001c5826d6ddcdfe8f3f2e5bd
alonronin <alon@ronin.co.il>
2020-04-12 13:36:52 +0300

    :wrench: remove sites, move boilerplate to projects, add examples submodule

--- sites/example.com/src/index.js
+++
@@ -1,8 +0,0 @@
-import $ from '@ampify/aQuery';
-import delay from '@ampify/toolbox/delay';
-
-export default () => {
-  console.log('aQuery goes here...');
-
-  return { cssIgnore: $.cssIgnore() };
-};
